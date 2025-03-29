fe0807bdf2ee82fddccabcee5a50a43b35e51089
Duc Ng <duc.nguyen@weallnet.com>
2025-03-28 22:13:06 -0400

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module plms_be
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+
+	// This is a simple Go program that prints "Hello, World!" to the console.
+	println("Hello, World!")
+	// The program ends here.
+}
